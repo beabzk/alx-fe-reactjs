394e683007d41c77e6d500446d7efeb3f3c142cd
Beabfekad Zikie <92110044+beabzk@users.noreply.github.com>
2024-08-23 10:07:00 +0300

    Add detailed recipe management

---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,26 @@
+import { useRecipeStore } from "./recipeStore";
+import EditRecipeForm from "./EditRecipeForm";
+import DeleteRecipeButton from "./DeleteRecipeButton";
+import { useParams } from "react-router-dom";
+
+const RecipeDetails = () => {
+  const { id } = useParams(); // Get the id from the URL params
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((recipe) => recipe.id === parseInt(id))
+  );
+
+  if (!recipe) {
+    return <p>Recipe not found</p>;
+  }
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipe.id} />
+    </div>
+  );
+};
+
+export default RecipeDetails;
