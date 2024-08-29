758ed5fe43a536ec2afb3a1b6fe199c764f488b0
Beabfekad Zikie <92110044+beabzk@users.noreply.github.com>
2024-08-29 17:45:55 +0300

    Add functionality to add new posts in PostsComponent

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,32 +1,33 @@
-import React from "react";
-import { useQuery } from "react-query";
-import axios from "axios";
-
-const fetchPosts = async () => {
-  const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-  return data;
-};
+import React, { useState } from "react";
+import { usePosts } from "./usePosts";
 
 function PostsComponent() {
-  const { data, isLoading, isError, error, refetch } = useQuery(
-    "posts",
-    fetchPosts,
-    {
-      staleTime: 60000, // 1 minute
-    }
-  );
+  const { posts, isLoading, isError, error, refetch, addPost } = usePosts();
+  const [newPostTitle, setNewPostTitle] = useState("");
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error: {error.message}</div>;
+
+  const handleAddPost = () => {
+    addPost({ title: newPostTitle, body: "New post body", userId: 1 });
+    setNewPostTitle("");
+  };
 
   return (
     <div>
       <h2>Posts</h2>
       <button onClick={() => refetch()}>Refetch Posts</button>
+      <div>
+        <input
+          type="text"
+          value={newPostTitle}
+          onChange={(e) => setNewPostTitle(e.target.value)}
+          placeholder="Enter new post title"
+        />
+        <button onClick={handleAddPost}>Add Post</button>
+      </div>
       <ul>
-        {data.map((post) => (
+        {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
       </ul>
