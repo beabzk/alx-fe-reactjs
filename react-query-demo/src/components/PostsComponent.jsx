ef02b632f70323d24ef2091a930afc6f11b810dd
Beabfekad Zikie <92110044+beabzk@users.noreply.github.com>
2024-08-29 17:51:41 +0300

    Add pagination functionality to PostsComponent

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
 
-const fetchPosts = async () => {
+const fetchPosts = async (page = 1) => {
   const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/posts"
+    `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`
   );
   return data;
 };
 
 function PostsComponent() {
+  const [page, setPage] = useState(1);
+
   const { data, isLoading, isError, error, refetch } = useQuery(
-    "posts",
-    fetchPosts,
+    ["posts", page],
+    () => fetchPosts(page),
     {
       staleTime: 60000, // 1 minute
+      cacheTime: 300000, // 5 minutes
+      refetchOnWindowFocus: false,
+      keepPreviousData: true,
     }
   );
 
@@ -26,6 +31,16 @@ function PostsComponent() {
       <h2>Posts</h2>
       <button onClick={() => refetch()}>Refetch Posts</button>
       <ul>{data && data.map((post) => <li key={post.id}>{post.title}</li>)}</ul>
+      <div>
+        <button
+          onClick={() => setPage((old) => Math.max(old - 1, 1))}
+          disabled={page === 1}
+        >
+          Previous Page
+        </button>
+        <span>Current Page: {page}</span>
+        <button onClick={() => setPage((old) => old + 1)}>Next Page</button>
+      </div>
     </div>
   );
 }
