be515c6a10aae2419a749dabdc1b93d143ce9f26
Beabfekad Zikie <92110044+beabzk@users.noreply.github.com>
2024-09-10 10:31:43 +0300

    refactor: Update RecipeDetail component layout and styling

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -8,7 +8,9 @@ const RecipeDetail = () => {
 
   useEffect(() => {
     const fetchRecipe = () => {
+      console.log("Data:", data); // Debugging line
       const recipeData = data.find((recipe) => recipe.id === parseInt(id));
+      console.log("Recipe Data:", recipeData); // Debugging line
       setRecipe(recipeData);
     };
 
@@ -20,22 +22,41 @@ const RecipeDetail = () => {
   }
 
   return (
-    <div>
-      <h1>{recipe.title}</h1>
-      <p>{recipe.summary}</p>
-      <img src={recipe.image} alt={recipe.title} />
-      <h2>Ingredients</h2>
-      <ul>
-        {recipe.ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
-        ))}
-      </ul>
-      <h2>Instructions</h2>
-      <ol>
-        {recipe.instructions.map((instruction, index) => (
-          <li key={index}>{instruction}</li>
-        ))}
-      </ol>
+    <div className="m-6">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center">
+        {recipe.title}
+      </h1>
+
+      <div className="m-3">
+        <img src={recipe.image} alt={recipe.title} className="mx-auto mb-4" />
+        <p className="text-center text-sm sm:text-base md:text-lg lg:text-xl">
+          {recipe.summary}
+        </p>
+      </div>
+
+      <div className="flex flex-col md:flex-row justify-between">
+        <div className="mb-4 md:mb-0">
+          <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl">
+            Ingredients
+          </h2>
+          <ul className="list-disc pl-5 text-sm sm:text-base md:text-lg lg:text-xl">
+            {recipe.ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl">
+            Instructions
+          </h2>
+          <ol className="list-decimal pl-5 text-sm sm:text-base md:text-lg lg:text-xl">
+            {recipe.instructions.map((instruction, index) => (
+              <li key={index}>{instruction}</li>
+            ))}
+          </ol>
+        </div>
+      </div>
     </div>
   );
 };
