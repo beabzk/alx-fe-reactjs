c57c308b9acd7030ba043b56d2983c71127cebed
Beabfekad Zikie <92110044+beabzk@users.noreply.github.com>
2024-09-21 07:07:54 +0300

    feat: Setup GitHub User Search React app

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GitHub User Search</h1>
+      </header>
+      <main>
+        {/* Search input will go here */}
+        <div className="search-container">
+          {/* Add search input component here */}
+        </div>
+        {/* Results will go here */}
+        <div className="results-container">
+          {/* Add results component here */}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
